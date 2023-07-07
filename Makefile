e2d7393b0a78d9806b03e5e2474a0efb167baedc
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-07-07 12:54:33 -0500

    Fix lint errors and add lint make target for running locally

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: test
-test: ## Run automated tests for IRSA module
+test: ## Run automated tests. Requires go and terraform to be installed locally.
 	cd test && go test . -v -count=1
+
+.PHONY: lint
+lint: ## Lint terraform files. Requires tflint to be installed locally.
+	tflint -f compact --recursive
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -15,15 +15,3 @@ variable "oidc_fully_qualified_subjects" {
   description = "The fully qualified OIDC subjects to be added to the role policy"
   default     = []
 }
-
-variable "role_permissions_boundary_arn" {
-  type        = string
-  description = "Permissions boundary ARN to use for IAM role"
-  default     = null
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "Map of tags"
-  default     = {}
-}
