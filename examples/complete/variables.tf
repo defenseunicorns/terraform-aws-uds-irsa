3e882c1e14cb92631f7a0b64e97332975c7dd809
Noah Birrer <noah@defenseunicorns.com>
2023-07-14 17:01:20 -0400

    fix linting issues in example

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,19 +1,7 @@
-variable "provider_url" {
-  type        = string
-  description = "OIDC provider URL"
-  default     = "oidc.eks.us-west-2.amazonaws.com/id/dummy-oidc-provider"
-}
-
 variable "name" {
   type        = string
   description = "Cluster name, used in the name of the iam role that is created"
   default     = "cluster-name"
-}
-
-variable "oidc_fully_qualified_subjects" {
-  type        = list(string)
-  description = "The fully qualified OIDC subjects to be added to the role policy"
-  default     = []
 }
 
 variable "kubernetes_namespace" {
